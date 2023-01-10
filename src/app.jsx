da4c27a376a12f6153a8a4eb33414bf189d04ac7
katyacremer <55757478+katyacremer@users.noreply.github.com>
2023-01-10 21:56:22 +0300

    HW-2

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
-import style from './style'
+import design from './design'
 import counter from './pages/counter'
 import GtAvt from './pages/GtAvt'
 
 const app = () => {
   return (
-    <Layout>
+    <design>
       <Routes>
         <Route path={"/"} element={<counter/>}/>
         <Route path={"/index"} element={<counter/>}/>
         <Route path={"/counter"} element={<counter/>}/>
         <Route path={"/gt_avt"} element={<GtAvt/>}/>
       </Routes>
-    </style>
+    </design>
   )
 }
 
