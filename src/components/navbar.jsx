10a6e61977c43941d136af58a4194cac40d0e75f
katyacremer <55757478+katyacremer@users.noreply.github.com>
2023-01-10 21:47:40 +0300

    HW-2

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import './styles/navbar.css'
+
+const navbar = () => {
+    return (
+      <nav className={"navbar_tab"}>
+        <h2 className={"navbar_text"}>Cremer Katerina BVT1901</h2>
+      </nav>
+    )
+}
+
+export default navbar;
