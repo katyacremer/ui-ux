032d6d154549363414a0a06fff56db962c2895a4
katyacremer <55757478+katyacremer@users.noreply.github.com>
2023-01-10 21:59:43 +0300

    HW-2

--- src/pages/GtAvt.jsx
+++ src/pages/GtAvt.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../components/styles/index.css'
+import '../styles/index.css'
 
 const GtAvt = () => {
     const [username, setUsername] = useState("");
